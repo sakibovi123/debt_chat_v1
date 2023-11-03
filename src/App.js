6c64a70de88d480d756eaf9e1cad1a59ebf439a8
sakibovi123 <sakibovi123@gmail.com>
2023-11-04 00:19:34 +0600

    stashes

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import Home from './Pages/Home';
 function App() {
   return (
     <div className="App">
-      <Routes>
+      <Home/>
+      {/* <Routes>
         <Route exact path="/" element={<Home />} />
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
